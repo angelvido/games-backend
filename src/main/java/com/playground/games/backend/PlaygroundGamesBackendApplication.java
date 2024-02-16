ff0ee883fc32ad85019e4dc34e46d3150f138c89
angelvido <vidaldominguezangel@gmail.com>
2024-02-16 14:22:48 +0100

    Primer commit

---
+++ src/main/java/com/playground/games/backend/PlaygroundGamesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.playground.games.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundGamesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundGamesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playground/games/backend/PlaygroundGamesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.games.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundGamesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
